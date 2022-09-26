fa1ac219447e1ee5661ea3725b7dc8690b550f20
Grzesiek <g.durtan@temida.io>
2022-09-26 19:12:24 +0200

    init files

---
+++ template/src/api/postmeta/routes/postmeta.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * postmeta router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::postmeta.postmeta');
---
+++ template/src/api/postmeta/services/postmeta.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * postmeta service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::postmeta.postmeta');
